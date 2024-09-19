ffa920bc453027b062e2d55d64ba2e0ef45b01bf
burning-monk <18663097+burning-monk@users.noreply.github.com>
2024-09-20 00:45:19 +0200

    add projects

--- main.js
+++ main.js
@@ -17,15 +17,46 @@
       repoLink: "https://github.com/Jan-Dev0/to-do-list",
       previewLink: "https://jan-dev0.github.io/to-do-list/",
     },
-    // Weitere Projekte hier
+    {
+      title: "Time tracking dashboard",
+      description: "",
+      image: "./images/time-tracking-dashboard.png",
+      languages: ["html5", "css3-alt", "js-square"],
+      repoLink: "https://github.com/Jan-Dev0/14-time-tracking-dashboard",
+      previewLink: "https://jan-dev0.github.io/14-time-tracking-dashboard/",
+    },
+    {
+      title: "Newsletter sign-up form",
+      description: "",
+      image: "./images/newsletter-signup.png",
+      languages: ["html5", "css3-alt", "js-square"],
+      repoLink: "https://github.com/Jan-Dev0/13-newsletter-sign-up-form",
+      previewLink: "https://jan-dev0.github.io/13-newsletter-sign-up-form/",
+    },
+    {
+      title: "Workit landing page",
+      description: "",
+      image: "./images/workit-landing-page.png",
+      languages: ["html5", "css3-alt", "js-square"],
+      repoLink: "https://github.com/Jan-Dev0/12-workit-landing-page",
+      previewLink: "https://jan-dev0.github.io/12-workit-landing-page/",
+    },
+    {
+      title: "Results summary component",
+      description: "",
+      image: "./images/results-summary-component.png",
+      languages: ["html5", "css3-alt", "js-square"],
+      repoLink: "https://github.com/Jan-Dev0/11-results-summary-component",
+      previewLink: "https://jan-dev0.github.io/11-results-summary-component/",
+    },
   ];
 
-  // Funktion zum Erzeugen der Karten
+ 
   function createProjectCards(projects) {
     const grid = document.querySelector('.projects-grid');
     
     projects.forEach(project => {
-      // Erstelle die einzelnen Karten-Elemente
+      
       const card = document.createElement('div');
       card.className = 'project-tile';
       
@@ -36,23 +67,40 @@
           </div>
           <div class="card-content">
             <h3 class="card-title">${project.title}</h3>
-            <p class="card-description">${project.description}</p>
             <div class="card-languages">
               ${project.languages.map(lang => `<i class="fab fa-${lang}" title="${lang}"></i>`).join('')}
             </div>
             <div class="card-links">
               <a href="${project.repoLink}" target="_blank" class="card-link card-link-left">Code</a>
-              <a href="${project.previewLink}" target="_blank" class="card-link card-link-right">Vorschau</a>
+              <a href="${project.previewLink}" target="_blank" class="card-link card-link-right">Preview</a>
             </div>
           </div>
         </div>
       `;
       
-      // Füge die Karte zum Grid hinzu
+    
       grid.appendChild(card);
     });
   }
 
-  // Rufe die Funktion auf, um die Karten zu erstellen
+  
   createProjectCards(projects);
+
+
+  const tabItems = document.querySelectorAll('.tab-item');
+const tabPanels = document.querySelectorAll('.tab-panel');
+
+tabItems.forEach(item => {
+  item.addEventListener('click', () => {
+    // Remove active class from all items and panels
+    tabItems.forEach(item => item.classList.remove('active'));
+    tabPanels.forEach(panel => panel.classList.remove('active'));
+
+    // Add active class to the clicked item and the corresponding panel
+    item.classList.add('active');
+    const tabPanel = document.getElementById(item.dataset.tab);
+    tabPanel.classList.add('active');
+  });
+});
+
 
